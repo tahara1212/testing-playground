1c45b5332443ac7dea030d5faadc8f3399ecb23f
tahara1212 <shunpei.tahara@gmail.com>
2025-01-07 15:26:27 +0900

    coverageの設定

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,19 @@ module.exports = {
     transform: {
       "node_modules/three/examples/.+.(j|t)sx?$": "ts-jest",
     },
-    testEnvironment: "jest-environment-jsdom", // or node
+    testEnvironment: "jest-environment-jsdom", 
     setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  };
-  
+    collectCoverage: true,
+    collectCoverageFrom: [
+      "src/components/**/*.{js,jsx,ts,tsx}", 
+    ],
+    coverageDirectory: "coverage", 
+    coverageThreshold: {
+      global: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
+  }
+}
